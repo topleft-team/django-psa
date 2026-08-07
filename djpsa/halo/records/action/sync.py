90772aaf512dcc7d7d9be6165b704ae8a3366839
Matt Fox <matt@topleft.team>
2026-08-07 16:27:36 -0700

    revert chargerate change

--- djpsa/halo/records/action/sync.py
+++ djpsa/halo/records/action/sync.py
@@ -22,7 +22,7 @@ class ActionSynchronizer(sync.ResponseKeyMixin,
         'project_id': (models.TicketTracker, 'project'),
         'who_agentid': (models.Agent, 'agent'),
         'outcome_id': (models.Outcome, 'outcome'),
-        'charge_rate_id': (models.ChargeRate, 'charge_rate'),
+        'charge_rate_id': (models.ChargeRate, 'chargerate'),
     }
 
     def _get_real_action_id(self, action_record):
